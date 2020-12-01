82a63720d9621670560c290b09f8861e93aa333c
Gabriel <gabriel-araujo52@hotmail.com>
2020-12-01 18:23:42 -0300

    Finalizando site

--- login/views.py
+++ login/views.py
@@ -8,14 +8,17 @@ import json
 
 def login(request):
     context = {
-        'noticias': 'a' 
+        'noticias': 'a'
     }
     return render(request, 'login.html', context)
 
+
 def login_site_seguro(request):
+    storage = messages.get_messages(request)
+    storage.used = True
     if request.method == 'POST':
-        user = request.POST.get('user','')
-        password = request.POST.get('password','')
+        user = request.POST.get('user', '')
+        password = request.POST.get('password', '')
         try:
             user = UserInseguro.objects.get(user=user, password=password)
         except:
@@ -24,21 +27,26 @@ def login_site_seguro(request):
             messages.add_message(request, messages.INFO, user)
             return redirect(reverse('site-seguro'))
     context = {
-        'message': 'site insguro' 
+        'message': 'site seguro'
     }
     return render(request, 'login.html', context)
 
+
 def login_site_inseguro(request):
+    storage = messages.get_messages(request)
+    storage.used = True
     if request.method == 'POST':
-        user = request.POST.get('user','')
-        password = request.POST.get('password','')
-        query = UserInseguro.objects.raw(f"SELECT * FROM login_userinseguro WHERE login_userinseguro.user='{user}' AND login_userinseguro.password='{password}'")
+        user = request.POST.get('user', '')
+        password = request.POST.get('password', '')
+        query = UserInseguro.objects.raw(
+            f"SELECT * FROM login_userinseguro WHERE login_userinseguro.user='{user}' AND login_userinseguro.password='{password}'"
+        )
         if query:
             user = query[0]
             messages.add_message(request, messages.INFO, user)
             return redirect(reverse('site-inseguro'))
     context = {
-        'message': 'site insguro' 
+        'message': 'site inseguro'
     }
     return render(request, 'login.html', context)
 
@@ -52,7 +60,7 @@ def comentarios_site_seguro(request):
     storage.used = False
 
     if request.method == 'POST':
-        comment = request.POST.get('comment',None)
+        comment = request.POST.get('comment', None)
         if comment:
             comment = Comment(user=user, comment=comment)
             comment.save()
@@ -60,13 +68,14 @@ def comentarios_site_seguro(request):
 
     all_comments = Comment.objects.all()[::-1]
     context = {
-        'img':5,
+        'img': 5,
         'site_seguro': True,
         'comments': all_comments,
         'user': user,
-        'message': ' essa é uma sessão de comentários segura, pode ficar Tranquilo.😎' 
+        'message': ' essa é uma sessão de comentários segura, pode ficar Tranquilo.😎'
     }
     return render(request, 'comments.html', context)
+
 
 def comentarios_site_inseguro(request):
     storage = messages.get_messages(request)
@@ -77,7 +86,7 @@ def comentarios_site_inseguro(request):
     storage.used = False
 
     if request.method == 'POST':
-        comment = request.POST.get('comment',None)
+        comment = request.POST.get('comment', None)
         if comment:
             comment = Comment(user=user, comment=comment)
             comment.save()
@@ -85,9 +94,9 @@ def comentarios_site_inseguro(request):
 
     all_comments = Comment.objects.all()[::-1]
     context = {
-        'img':6,
+        'img': 6,
         'comments': all_comments,
         'user': user,
-        'message': ' essa é uma sessão de comentários insegura, CUIDADO!!! 😱' 
+        'message': ' essa é uma sessão de comentários insegura, CUIDADO!!! 😱'
     }
     return render(request, 'comments.html', context)
